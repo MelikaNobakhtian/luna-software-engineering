8a414bf56eb90ba4539e8a0f49f09229033b356c
haminob48 <haminob48@gmail.com>
2021-03-14 22:35:28 -0700

    Create postgres db and env file

--- Backend/BackProject/BackProject/BackProject/settings.py
+++ Backend/BackProject/BackProject/BackProject/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v3n8s!(_&tde$iov_j3l@mc6zmu(b8v!j#lbwbbd)6vi7ouuie'
+SECRET_KEY = env("SECRET_KEY")
+#SECRET_KEY = 'v3n8s!(_&tde$iov_j3l@mc6zmu(b8v!j#lbwbbd)6vi7ouuie'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'basicapp',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +85,16 @@ WSGI_APPLICATION = 'BackProject.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': env("DATABASE_NAME"),
+         'USER': env("DATABASE_USER"),
+         'PASSWORD': env("DATABASE_PASSWORD"),
+         'HOST': env("DATABASE_HOST"),
+         'PORT': env("DATABASE_PORT"),
+     }
+
 }
 
 
