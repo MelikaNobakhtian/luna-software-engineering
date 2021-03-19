6250e2ed39f3a6b510d99a3151b2d63b286b5d00
CE9752 <fateme.ahmadi1522@gmail.com>
2021-03-19 12:00:12 +0330

    Implement register doctor view

--- Backend/BackProject/BackProject/basicapp/views.py
+++ Backend/BackProject/BackProject/basicapp/views.py
@@ -37,6 +37,28 @@ class RegisterView(generics.GenericAPIView):
         Util.send_email(data)
         return Response(user_data, status=status.HTTP_201_CREATED)
 
+class RegisterDoctorView(generics.GenericAPIView):
+
+    serializer_class = RegisterDoctorSerializer
+
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+        user = DoctorUser.objects.get(email=user_data['email'])
+        token = RefreshToken.for_user(user).access_token
+        current_site = get_current_site(request).domain
+        relativeLink = reverse('email-verify')
+        absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+        email_body = 'Hi '+user.first_name+' '+user.last_name + \
+            ' Use the link below to verify your email \n' + absurl
+        data = {'email_body': email_body, 'to_email': user.email,
+                'email_subject': 'Verify your email'}
+
+        Util.send_email(data)
+        return Response(user_data, status=status.HTTP_201_CREATED)
 
 class VerifyEmail(views.APIView):
 
