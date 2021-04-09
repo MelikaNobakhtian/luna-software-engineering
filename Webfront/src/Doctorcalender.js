ab8a8a0a08828aa01e88f987ec2170bcd9c946b1
setayeshk <setayeshk12345@gmail.com>
2021-04-09 19:42:36 +0430

    Install react-datepicker2

--- Webfront/src/Doctorcalender.js
+++ Webfront/src/Doctorcalender.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
-import { DatePicker } from "jalali-react-datepicker";
+// import { DatePicker } from "jalali-react-datepicker";
 import { RestaurantMenu } from "@material-ui/icons";
+import moment from 'moment-jalaali'
+import DatePicker from 'react-datepicker2';
 
 
 function Doctorcalender(){
