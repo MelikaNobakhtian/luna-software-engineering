6c70c4bc4efcd40c83351b26af1713a1f871e180
setayeshk <setayeshk12345@gmail.com>
2021-04-10 17:15:07 +0430

    Add text and input for doctors sessions duration

--- Webfront/src/components/doctortimes/Doctorcalender.js
+++ Webfront/src/components/doctortimes/Doctorcalender.js
@@ -6,14 +6,51 @@ import moment from 'moment-jalaali'
 import DatePicker from 'react-datepicker2';
 import Navbar from "../../Navbar"
 
-function Doctorcalender(){
-    return(
-        <div>
-        <Navbar></Navbar>
-      <div style={{}}>مدت زمان وقت حضوری شما؟</div>
-      <div>مدت زمان وقت مجازی شما؟</div>
-        <DatePicker></DatePicker>
+import { Toast, Button, Form, FormGroup, Label, input, FormText, Col, InputGroup } from 'react-bootstrap';
+
+function Doctorcalender() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h1> سلام</h1>
+      <p>
+        برای انتخاب وقت های حضوری و یا مجازی خود ابتدا روز مورد نظر خود را از روی تقویم انتخاب کرده و سپس بازه ی زمانی ای که در آن میتوانید نوبت حضوری داشته باشید را در کادر بازه های نوبت حضوری و مدت زمانی را که میتونید در آن وقت مجازی داشته باشید در کادر بازه های نوبت مجازی وارد کنید
+        </p>
+
+
+      <div class="row g-3 align-items-center col-4 h-25">
+        <div class="col-auto">
+          <label for="hozori" class="col-form-label small ">مدت زمان هر وقت حضوری شما؟</label>
         </div>
+        <div class="col-2">
+        <input type="password" id="hozori" class="form-control" aria-describedby="passwordHelpInline"></input>
+        </div>
+        </div>
+
+        <div class="row">
+          <div class="cal-3">j</div>
+          <label for="hozori" class="col-form-label">مدت زمان هر وقت حضوری؟</label>
+          <input type="text ratio" class="form-control" id="hozori" style={{ borderRadius: 100 }}></input>
+        </div>
+
+        <div>مدت زمان وقت مجازی شما؟
+      <input></input>
+        </div>
+        <DatePicker></DatePicker>
+        
+        {/* <div class="row">
+      
+        {/* "calc(1em + 0.1vw)" */}
+        {/* <h3 class="col-2" */}
+        {/* //  style={{fontSize:"calc(0.8em + 0.1vw)"}}
+//          >مدت زمان وقت مجازی شما چه قدر می باشد ؟؟؟</h3> */}
+        {/* //          <h3> HI </h3>
+//         <div class="col-6 me-n5" style={{backgroundColor:"blue"}}> jj</div>
+//         </div> */}
+        {/* // <div>dfkdflkdf;lkj</div> */}
+
+      </div>
+
 
     )
 }
