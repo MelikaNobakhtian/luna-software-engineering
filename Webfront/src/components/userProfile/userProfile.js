3abe81676b81ae2c6b3b34ea55ed38906ec6dc1a
fomid8 <fati.omid2000@gmail.com>
2021-04-11 19:01:05 +0430

    initiate user profile page

---
+++ Webfront/src/components/userProfile/userProfile.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import {API_BASE_URL} from '../apiConstant/apiConstant';
+
+function App() { 
+    
+    
+    
+  return(
+
+
+
+  );
+}
+export default withRouter(SignUp);
