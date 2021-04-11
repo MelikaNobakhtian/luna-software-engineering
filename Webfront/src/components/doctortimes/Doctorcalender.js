1dbab23fff5660b4ba54dae25198f76b56ee7695
setayeshk <setayeshk12345@gmail.com>
2021-04-11 18:43:53 +0430

    Edit doctor calender ui

--- Webfront/src/components/doctortimes/Doctorcalender.js
+++ Webfront/src/components/doctortimes/Doctorcalender.js
@@ -1,6 +1,6 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
- import {RangeDatePicker  } from "jalali-react-datepicker";
+import { RangeDatePicker } from "jalali-react-datepicker";
 import { RestaurantMenu } from "@material-ui/icons";
 import moment from 'moment-jalaali'
 // import DatePicker from 'react-datepicker2';
@@ -15,60 +15,77 @@ function Doctorcalender() {
     from: null,
     to: null
   });
+  const calenderchange=(value)=>{
+    console.log("hi")
+    console.log(value +"value")
+    console.log(value.from.year+"year"+value.from.month+"month"+value.from.day+" _ "+value.to)
+    setSelectedDayRange(value)
+  }
   return (
     <div >
       <Navbar></Navbar>
       {/* وقت absolute dige flex end o ina taghiri ijad na */}
       {/* <div class="d-flex flex-row col-10 col-sm-2 mt-5" style={{justifyContent:"flex-end"}}> */}
 
-      <div class="d-flex p-2 flex-row-reverse  bd-highlight mt-1" style={{backgroundColor:"blue"}}>
-      hi
-      <div  class="order-5-lg ms-5-lg">
-        <Calendar
-        calendarClassName="responsive-calendar" 
-       value={selectedDayRange}
-      onChange={setSelectedDayRange}
-      // shouldHighlightWeekends
-      locale="fa" // add this
-    />
-    </div>
-    </div>
+      <div class="d-flex p-2 flex-row-reverse bd-highlight" style={{ backgroundColor: "white" }}>
+        {/* hi */}
+        <div class="order-5-lg col-lg-6 mt-sm-5 mt-lg-5 mt-xl-5 mt-xxl-5 mt-md-5 responsive-calendar" style={{borderWidth:20,padding:1,borderRadius:10, borderColor:"lightskyblue",backgroundColor:"lightskyblue"}}>
+          <Calendar
+            // backgroundColor="green"
+            // theme="dark"
+            // background-image="blue"
+            // style={{ backgroundColor: "green" }}
+            shouldHighlightWeekends="true"
+            calss=".shadow-0"
+            colorPrimary="#0fbcf9"
+            colorPrimaryLight="rgba(75, 207, 250, 0.4)"
+            calendarClassName="responsive-calendar"
+            value={selectedDayRange}
+            onChange={
+              (value)=>calenderchange(value)
+              
+            }
+            // shouldHighlightWeekends
+            locale="fa" // add this
+          />
+        </div>
+      </div>
       {/* <h1> سلام</h1>
       
       <p className="sessionstime">
         برای انتخاب وقت های حضوری و یا مجازی خود ابتدا روز مورد نظر خود را از روی تقویم انتخاب کرده و سپس بازه ی زمانی ای که در آن میتوانید نوبت حضوری داشته باشید را در کادر بازه های نوبت حضوری و مدت زمانی را که میتونید در آن وقت مجازی داشته باشید در کادر بازه های نوبت مجازی وارد کنید
         </p> */}
- 
-        <div class="col-6 row-6" style={{backgroundColor:"lightgreen",height:300}}>h </div>
+
+      {/* <div class="col-6 row-6" style={{ backgroundColor: "white", height: 300 }}>h </div> */}
       <div class="row me-0 ms-0 align-items-center col-auto ">
         <div class="col-auto">
           <label for="hozori" class="col-auto ms-n3 sessionstime ">مدت زمان هر وقت حضوری شما؟</label>
         </div>
         {/* class="col-2 me-n3 " */}
-        <div class=" col-auto row" 
+        <div class=" col-auto row"
         // style={{height:"clamp(10px,4vh,60px)" , width:"clamp(20px,4.5vw,40px)",borderRadius:100,backgroundColor:"white"}} 
         >
-        {/* //width toye screen bozorg yeho ziadi ziad vali height taghriban hammon */}
-        {/* نوشته ی توش ریسپانسیو کوچیک نمیشه */}
-        <input  id="hozori" class="col-auto" class="form-control" style={{height:"clamp(10px,4.5vh,65px)" , width:"clamp(20px,5.5vw,45px)",borderRadius:100,backgroundColor:"white"}} aria-describedby="passwordHelpInline"></input>
+          {/* //width toye screen bozorg yeho ziadi ziad vali height taghriban hammon */}
+          {/* نوشته ی توش ریسپانسیو کوچیک نمیشه */}
+          <input id="hozori" class="col-auto" class="form-control" style={{ height: "clamp(10px,4.5vh,65px)", width: "clamp(20px,5.5vw,45px)", borderRadius: 100, backgroundColor: "white" }} aria-describedby="passwordHelpInline"></input>
         </div>
-        </div>
+      </div>
 
-        <div class="row me-0 ms-0 align-items-center col-auto mt-4 ">
+      <div class="row me-0 ms-0 align-items-center col-auto mt-4 ">
         <div class="col-auto">
           <label for="hozori" class="col-auto ms-n3 sessionstime ">مدت زمان هر وقت مجازی شما؟</label>
         </div>
         {/* class="col-2 me-n3 " */}
-        <div class=" col-auto row" 
+        <div class=" col-auto row"
         // style={{height:"clamp(10px,4vh,60px)" , width:"clamp(20px,4.5vw,40px)",borderRadius:100,backgroundColor:"white"}} 
         >
-        {/* //width toye screen bozorg yeho ziadi ziad vali height taghriban hammon */}
-        {/* نوشته ی توش ریسپانسیو کوچیک نمیشه */}
-        <input  id="hozori" class="col-auto" class="form-control" style={{height:"clamp(10px,4.5vh,65px)" , width:"clamp(20px,5.5vw,45px)",borderRadius:100,backgroundColor:"white"}} aria-describedby="passwordHelpInline"></input>
+          {/* //width toye screen bozorg yeho ziadi ziad vali height taghriban hammon */}
+          {/* نوشته ی توش ریسپانسیو کوچیک نمیشه */}
+          <input id="hozori" class="col-auto" class="form-control" style={{ height: "clamp(10px,4.5vh,65px)", width: "clamp(20px,5.5vw,45px)", borderRadius: 100, backgroundColor: "white" }} aria-describedby="passwordHelpInline"></input>
         </div>
-        </div>
+      </div>
 
-        {/* <div >
+      {/* <div >
           <div class="cal-3">j</div>
           <label for="hozori" class="col-form-label">مدت زمان هر وقت حضوری؟</label>
           <input type="text ratio" class="form-control" id="hozori" style={{ borderRadius: 100 }}></input>
@@ -77,24 +94,24 @@ function Doctorcalender() {
         <div>مدت زمان وقت مجازی شما؟
       <input></input>
         </div> */}
-        {/* <div class="d-flex p-2 bd-highlight" style={{position:"absolute", alignSelf:"flex-start",justifySelf:"flex-end",backgroundColor:"blue"}}> */}
-       
-        <RangeDatePicker></RangeDatePicker>
-        {/* </div> */}
-        {/* <div class="row">
+      {/* <div class="d-flex p-2 bd-highlight" style={{position:"absolute", alignSelf:"flex-start",justifySelf:"flex-end",backgroundColor:"blue"}}> */}
+
+      <RangeDatePicker></RangeDatePicker>
+      {/* </div> */}
+      {/* <div class="row">
       
         {/* "calc(1em + 0.1vw)" */}
-        {/* <h3 class="col-2" */}
-        {/* //  style={{fontSize:"calc(0.8em + 0.1vw)"}}
+      {/* <h3 class="col-2" */}
+      {/* //  style={{fontSize:"calc(0.8em + 0.1vw)"}}
 //          >مدت زمان وقت مجازی شما چه قدر می باشد ؟؟؟</h3> */}
-        {/* //          <h3> HI </h3>
+      {/* //          <h3> HI </h3>
 //         <div class="col-6 me-n5" style={{backgroundColor:"blue"}}> jj</div>
 //         </div> */}
-        {/* // <div>dfkdflkdf;lkj</div> */}
+      {/* // <div>dfkdflkdf;lkj</div> */}
 
-      </div>
+    </div>
 
 
-    )
+  )
 }
 export default Doctorcalender;
