af3d8d24a6c6edf2a190be37f35d4d5aa0e39bea
fomid8 <fati.omid2000@gmail.com>
2021-04-12 06:43:29 +0430

    add ui forgot password page

--- Webfront/src/components/login/forgotPassword.js
+++ Webfront/src/components/login/forgotPassword.js
@@ -6,9 +6,37 @@ import {API_BASE_URL} from '../apiConstant/apiConstant';
 
 function ForgotPassword(props) {
 
+    useEffect(() => {
+        
+    },[] );
+
+    const changePassword = () =>{
+
+    }
+
   return(
       <div>
-          ForgotPassword
+          <div className="d-flex justify-content-center background">
+            <div className="  "  >
+              <div className="card border border-5 border-success shadow-lg" >
+                <div class="card-body ">
+                <form className="row g-3">
+                  <div>
+                    <label class="form-label"  >رمز جدید</label>
+                    <input type="password" class="form-control"  placeholder="رمز جدید خود را وارد کنید"/>
+                  </div>
+                  <div >
+                    <label class="form-label"  >تکرار رمز جدید</label>
+                    <input type="password" class="form-control"  placeholder="رمز جدید خود را دوباره وارد کنید"/>
+                  </div>
+                  
+                </form>
+                <button type="submit" class="btn btn-success mt-3" onClick={changePassword}>تایید</button>
+
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
 
 
