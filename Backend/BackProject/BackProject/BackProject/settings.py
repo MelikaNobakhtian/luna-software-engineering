4ddad19cd4e7c53f0a84fccae871ad16675eb152
fateme.ahmadi1522 <fateme.ahmadi1522@gmail.com>
2021-04-15 20:55:32 +0000

    Updated settings.py

--- Backend/BackProject/BackProject/BackProject/settings.py
+++ Backend/BackProject/BackProject/BackProject/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
-#SECRET_KEY = 'v3n8s!(_&tde$iov_j3l@mc6zmu(b8v!j#lbwbbd)6vi7ouuie'
+#SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'v3n8s!(_&tde$iov_j3l@mc6zmu(b8v!j#lbwbbd)6vi7ouuie'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
