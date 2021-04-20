a142647371e00e2c3aa0546dc79f040042062e65
fomid8 <fati.omid2000@gmail.com>
2021-04-20 19:46:51 +0430

    Add show userprofile page only with token

--- Webfront/src/components/userProfile/userProfile.js
+++ Webfront/src/components/userProfile/userProfile.js
@@ -22,24 +22,24 @@ function UserProfile(props) {
   });
 
   useEffect(() => {
-    //if (user.token) {
-    axios
-      .get(API_BASE_URL + "/user/" + Cookies.get("userId"))
-      .then(function (response) {
-        console.log(response);
-        setUser((prevState) => ({
-          ...prevState,
-          userName: response.data.username,
-          firstName: response.data.first_name,
-          lastName: response.data.last_name,
-          email: response.data.email,
-          picture: API_BASE_URL + response.data.profile_photo,
-        }));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    //}
+    if (!Cookies.get("userTokenA")) {
+      axios
+        .get(API_BASE_URL + "/user/" + Cookies.get("userId"))
+        .then(function (response) {
+          console.log(response);
+          setUser((prevState) => ({
+            ...prevState,
+            userName: response.data.username,
+            firstName: response.data.first_name,
+            lastName: response.data.last_name,
+            email: response.data.email,
+            picture: API_BASE_URL + response.data.profile_photo,
+          }));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
   }, []);
 
   const handleChangeInfosClick = (e) => {
@@ -88,7 +88,6 @@ function UserProfile(props) {
     }));
   };
 
-  
   const handleChangePassClick = (e) => {
     e.preventDefault();
     if (
@@ -146,7 +145,6 @@ function UserProfile(props) {
     }));
   };
 
-
   //get image from user
   const [state, setState] = useState({
     navigate: false,
@@ -182,178 +180,185 @@ function UserProfile(props) {
     setOpenSnack(false);
   };
 
-  const handleLogout =()=>{
+  const handleLogout = () => {
     Cookies.remove("userId");
     Cookies.remove("doctorId");
     Cookies.remove("userTokenR");
     Cookies.remove("userTokenA");
-    props.history.push('/login');
-  }
+    props.history.push("/login");
+  };
 
   return (
     <div className="main-content ">
       <div className="container-fluid p-2">
-        <div className="d-flex flex-wrap">
-          <div className="col-12 col-md-4 ">
-            <div className="card border-dark border-2 text-white p-2 m-1 App-color4 shadow-lg">
-              <div class="card-header d-flex justify-content-center">
-                <div className="text-center">
-                  <Avatar
-                    src={user.picture}
-                    ref={uploadedImage}
-                    alt="عکس"
-                    className=""
-                    style={{ width: 120, height: 120 }}
-                  />
-                  <h5 className="username mt-2">{user.userName}</h5>
-                  <h6 className="email">{user.email}</h6>
+        {!Cookies.get("userTokenA") ? (
+          <div className="text-center">لطفا وارد حساب خود شوید</div>
+        ) : (
+          <div className="d-flex flex-wrap">
+            <div className="col-12 col-md-4 ">
+              <div className="card border-dark border-2 text-white p-2 m-1 App-color4 shadow-lg">
+                <div class="card-header d-flex justify-content-center">
+                  <div className="text-center">
+                    <Avatar
+                      src={user.picture}
+                      ref={uploadedImage}
+                      alt="عکس"
+                      className=""
+                      style={{ width: 120, height: 120 }}
+                    />
+                    <h5 className="username mt-2">{user.userName}</h5>
+                    <h6 className="email">{user.email}</h6>
+                  </div>
                 </div>
-              </div>
-              <div class="card-body flex row g-2">
-              <div className="btn btn-outline-light btn-sm" onClick={handleLogout}>
-                خروج از حساب
-                </div>
-                <div className="btn btn-outline-light btn-sm">
-                  <EditIcon></EditIcon>
-                  ویرایش اطلاعات
+                <div class="card-body flex row g-2">
+                  <div
+                    className="btn btn-outline-light btn-sm"
+                    onClick={handleLogout}
+                  >
+                    خروج از حساب
+                  </div>
+                  <div className="btn btn-outline-light btn-sm">
+                    <EditIcon></EditIcon>
+                    ویرایش اطلاعات
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col-12 col-md-8 ">
-            <div className="card border-dark border-2 text-white p-2 m-1 App-color4 shadow-lg">
-              <div class="card-header d-flex">
-                <h4>اطلاعات شخصی</h4>
-              </div>
-              <div class="card-body">
-                <div>
-                  <form class="row g-3">
-                    <div class="col-sm-4 ">
-                      <label for="firstName" class="form-label">
-                        نام
-                      </label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        id="firstName"
-                        value={user.firstName}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div class="col-sm-4">
-                      <label for="lastName" class="form-label">
-                        نام خانوادگی
-                      </label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        id="lastName"
-                        value={user.lastName}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div class="col-sm-4 ">
-                      <label for="userName" class="form-label">
-                        نام کاربری
-                      </label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        id="userName"
-                        value={user.userName}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div class="col-12 ">
-                      <input
-                        class="form-control"
-                        type="file"
-                        accept="image/*"
-                        onChange={handleImageUpload}
-                        ref={imageUploader}
-                        style={{ display: "none", color: "white" }}
-                      />
-                      <div
-                        className="btn btn-outline-light "
-                        onClick={() => imageUploader.current.click()}
-                      >
-                        <AddPhotoAlternateIcon></AddPhotoAlternateIcon>
-                        انتخاب عکس جدید
+            <div className="col-12 col-md-8 ">
+              <div className="card border-dark border-2 text-white p-2 m-1 App-color4 shadow-lg">
+                <div class="card-header d-flex">
+                  <h4>اطلاعات شخصی</h4>
+                </div>
+                <div class="card-body">
+                  <div>
+                    <form class="row g-3">
+                      <div class="col-sm-4 ">
+                        <label for="firstName" class="form-label">
+                          نام
+                        </label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="firstName"
+                          value={user.firstName}
+                          onChange={handleChange}
+                        />
                       </div>
-                    </div>
-                    <div class="col-12">
-                      <button
-                        type="submit"
-                        class="btn btn-outline-light"
-                        onClick={handleChangeInfosClick}
-                      >
-                        <CheckCircleIcon></CheckCircleIcon>
-                        ذخیره تغییرات
-                      </button>
-                    </div>
+                      <div class="col-sm-4">
+                        <label for="lastName" class="form-label">
+                          نام خانوادگی
+                        </label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="lastName"
+                          value={user.lastName}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div class="col-sm-4 ">
+                        <label for="userName" class="form-label">
+                          نام کاربری
+                        </label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="userName"
+                          value={user.userName}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div class="col-12 ">
+                        <input
+                          class="form-control"
+                          type="file"
+                          accept="image/*"
+                          onChange={handleImageUpload}
+                          ref={imageUploader}
+                          style={{ display: "none", color: "white" }}
+                        />
+                        <div
+                          className="btn btn-outline-light "
+                          onClick={() => imageUploader.current.click()}
+                        >
+                          <AddPhotoAlternateIcon></AddPhotoAlternateIcon>
+                          انتخاب عکس جدید
+                        </div>
+                      </div>
+                      <div class="col-12">
+                        <button
+                          type="submit"
+                          class="btn btn-outline-light"
+                          onClick={handleChangeInfosClick}
+                        >
+                          <CheckCircleIcon></CheckCircleIcon>
+                          ذخیره تغییرات
+                        </button>
+                      </div>
 
-                    <hr></hr>
+                      <hr></hr>
 
-                    <div class="col-sm-4">
-                      <label for="oldPass" class="form-label">
-                        رمز فعلی
-                      </label>
-                      <input
-                        type="password"
-                        class="form-control"
-                        id="oldPass"
-                        value={user.oldPass}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div class="col-sm-4">
-                      <label for="newPass" class="form-label">
-                        رمز جدید
-                      </label>
-                      <input
-                        type="password"
-                        class="form-control"
-                        id="newPass"
-                        value={user.newPass}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div class="col-sm-4">
-                      <label for="newPass2" class="form-label">
-                        تکرار رمز جدید
-                      </label>
-                      <input
-                        type="password"
-                        class="form-control"
-                        id="newPass2"
-                        value={user.newPass2}
-                        onChange={handleChange}
-                      />
-                    </div>
+                      <div class="col-sm-4">
+                        <label for="oldPass" class="form-label">
+                          رمز فعلی
+                        </label>
+                        <input
+                          type="password"
+                          class="form-control"
+                          id="oldPass"
+                          value={user.oldPass}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div class="col-sm-4">
+                        <label for="newPass" class="form-label">
+                          رمز جدید
+                        </label>
+                        <input
+                          type="password"
+                          class="form-control"
+                          id="newPass"
+                          value={user.newPass}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <div class="col-sm-4">
+                        <label for="newPass2" class="form-label">
+                          تکرار رمز جدید
+                        </label>
+                        <input
+                          type="password"
+                          class="form-control"
+                          id="newPass2"
+                          value={user.newPass2}
+                          onChange={handleChange}
+                        />
+                      </div>
 
-                    <div class="col-12">
-                      <button
-                        type="submit"
-                        class="btn btn-outline-light"
-                        onClick={handleChangePassClick}
-                      >
-                        <CheckCircleIcon></CheckCircleIcon>
-                        تغییر رمز
-                      </button>
-                    </div>
-                  </form>
+                      <div class="col-12">
+                        <button
+                          type="submit"
+                          class="btn btn-outline-light"
+                          onClick={handleChangePassClick}
+                        >
+                          <CheckCircleIcon></CheckCircleIcon>
+                          تغییر رمز
+                        </button>
+                      </div>
+                    </form>
+                  </div>
                 </div>
               </div>
             </div>
+            <Snackbar
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+              open={openSnack}
+              autoHideDuration={2500}
+              onClose={handleCloseSnack}
+              message={<div style={{ fontSize: 17 }}>{massage}</div>}
+            />
           </div>
-          <Snackbar
-            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-            open={openSnack}
-            autoHideDuration={2500}
-            onClose={handleCloseSnack}
-            message={<div style={{ fontSize: 17 }}>{massage}</div>}
-          />
-        </div>
+        )}
       </div>
     </div>
   );
