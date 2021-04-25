95b3727b1fb6981aa0336b401a2ce41857d46c1f
fomid8 <fati.omid2000@gmail.com>
2021-04-25 19:49:20 +0430

    Add ui for doctor search results

--- Webfront/src/components/search/searchResult.js
+++ Webfront/src/components/search/searchResult.js
@@ -4,7 +4,9 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import { API_BASE_URL } from "../apiConstant/apiConstant";
 import { Avatar } from "@material-ui/core";
-
+import GradeRoundedIcon from "@material-ui/icons/GradeRounded";
+import { RiUserStarFill } from "react-icons/all";
+import { FcApproval } from "react-icons/fc";
 function SearchResult(props) {
   const [doctors, setDoctors] = useState([]);
   const [page, setPage] = useState(1);
@@ -30,11 +32,75 @@ function SearchResult(props) {
       });
   }, [props.match.params.search, page]);
 
+  const routeToDoctorHandler = () => {
+    props.history.push("/visitDoctor");
+  };
+
   return (
     <div className="main-content">
       <div className="container-fluid p-2">
         <div className="d-flex flex-wrap">
-            
+          <div className="mx-md-5 px-md-5">
+            {doctors.length === 0 ? (
+              <p>نتیجه‌ای برای نمایش وجود ندارد</p>
+            ) : (
+              <div
+                class="row row-cols-1 row-cols-md-4 row-cols-sm-2"
+                style={{ textAlign: "right" }}
+              >
+                {doctors.map((current) => (
+                  <div class="col mb-4">
+                    <div class="card h-100 shadow">
+                      <Avatar
+                        src={current.doctor_photo}
+                        class="card-img-top shadow-sm "
+                        alt={current.name}
+                        onClick={() => routeToDoctorHandler(current.id)}
+                      />
+                      <div class="card-body">
+                        <h5
+                          class="card-title btn m-n2"
+                          onClick={() => routeToDoctorHandler(current.id)}
+                          style={{ fontSize: 25 }}
+                        >
+                          {current.firstname}
+                          {current.lastname}
+                        </h5>
+                        <p class="card-text">{current.specialty}</p>
+                      </div>
+                      <div className="align-items-center m-3">
+                        <h6 class="card-subtitle mt-1 text-muted">
+                          {current.score}
+                        </h6>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+
+          <div className="mb-5">
+            {pagesNumber === 1 || pagesNumber === undefined ? (
+              <p></p>
+            ) : (
+              <div>
+                {Array.from(Array(pagesNumber), (e, i) => {
+                  return (
+                    <div
+                      className="btn btn-light"
+                      onClick={() => {
+                        setPage(i + 1);
+                      }}
+                    >
+                      {" "}
+                      {i + 1}{" "}
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
