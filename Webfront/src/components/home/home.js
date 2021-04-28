d1a082d01c2bb382e225ecd597eed49aa964dcb3
Saba Rayehi <S.Rayehi@gmail.com>
2021-04-28 21:05:29 +0430

    Add list of doctor in homepage (show information)

--- Webfront/src/components/home/home.js
+++ Webfront/src/components/home/home.js
@@ -1,11 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { withRouter } from "react-router-dom";
-// import axios from "axios";
-// import Cookies from "js-cookie";
-//import { API_BASE_URL } from "../apiConstant/apiConstant";
+  import React, { useState, useEffect } from "react";
+  import { withRouter } from "react-router-dom";
+  import { Card, CardColumns,CardImg ,Row} from "react-bootstrap";
+  import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap'
+ 
+  
+  function Home(props) {
+    return (
+  <div>
+    
+  <Card style={{ width: '18rem' }}>
+  <Card.Img  class="rounded-circle z-depth-0" alt="20x20"    variant="top" src='https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGljfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'  data-holder-rounded="true"/>
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
 
-function Home(props) {
-  return <div>home</div>;
-}
+  <Card style={{ width: '18rem' }}>
+  <Card.Img  class="rounded-circle z-depth-0" alt="20x20"    variant="top" src='https://react.semantic-ui.com/images/avatar/large/matthew.png'  data-holder-rounded="true"/>
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
+  </div>
+  
+    )
+
+  }
+  
 
 export default withRouter(Home);
