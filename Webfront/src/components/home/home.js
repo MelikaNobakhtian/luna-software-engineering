773da7403f6dd684f6488a8ec1a6595c56db66d9
Saba Rayehi <S.Rayehi@gmail.com>
2021-04-29 15:28:00 +0430

    Change button material - add loading to button

--- Webfront/src/components/home/home.js
+++ Webfront/src/components/home/home.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import { Card, CardColumns, Img, Row } from "react-bootstrap";
-import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./home.css";
 import axios from "axios";
 import Cookies from "js-cookie";
 import { API_BASE_URL } from "../apiConstant/apiConstant";
 import { Avatar } from "@material-ui/core";
+import ReactiveButton from 'reactive-button';
 
 function Home(props) {
+  const [state, setState] = useState('idle');
   const [doctor, setDoctor] = useState([{
     "id": 15,
     "user": {
@@ -48,18 +50,27 @@ function Home(props) {
   }, []);
 
 
+
+
+  const onClickHandler = () => {
+      setState('loading');
+      setTimeout(() => {
+          setState('success');
+      }, 2000);
+  }
+
   return (
     <div>
 
       <div className="home">
         <div className="homePage" >
-          <div class="row row-cols-1 row-cols-md-3" >
+          <div class="row row-cols-1 row-cols-md-3"  >
             {doctor.length === 0 ? <div></div> :
               doctor.map((doc) => {
                 if (doc) return (
                   <div class="col mb-4">
-                    <Card style={{ width: '20rem' }}>
-                      <div class=" card-header d-flex justify-content-center">
+                    <Card style={{ width: '20rem'}} >
+                      <div class=" card-header d-flex justify-content-center" >
                         <Avatar
                           src={doc.user.profile_photo}
                           alt="عکس"
@@ -68,40 +79,48 @@ function Home(props) {
                         />
                       </div>
                       <Card.Body>
-                        <Card.Title>{"دکتر "+ doc.user.first_name + " " + doc.user.last_name}</Card.Title>
-                       
-      <div>
-                          <i class="fa fa-stethoscope" ></i>
-                      </div>
-                          <Card.Text>
-                          {/* <i class="fas fa-stethoscope"></i> */}
-                          <span class="input-group-prepend">
-    <div class="input-group-text bg-transparent border-0">
-      <i class="fa fa-search"></i>
-    </div>
-  </span>
-                          {"تخصص "+ doc.specialty}
+                        <Card.Title>{"دکتر " + doc.user.first_name + " " + doc.user.last_name}</Card.Title>
+
+                        <Card.Text>
+                          {"تخصص " + doc.specialty}
                         </Card.Text>
                         <Card.Text>
                           {doc.sub_specialty}
                         </Card.Text>
 
-           {doc.addresses.length === 0 ? <div></div> :
-              doc.addresses.map((address) => {
-                if (address) return (
-                        <Card.Text>
-                          {address.state +"،"+address.city+"،"+address.detail}
-                        </Card.Text>
-                          )
-                        })}
-                        
-                        <Button variant="primary">نوبت دهی</Button>
+                        {doc.addresses.length === 0 ? <div></div> :
+                          doc.addresses.map((address) => {
+                            if (address) return (
+                              <Card.Text>
+
+                                
+                                {"● "+address.state + "،" + address.city + "،" + address.detail}
+                              </Card.Text>
+                            )
+                          })}
+
+                        <ReactiveButton
+            buttonState={state}
+            onClick={onClickHandler}
+            color={'green'}
+            idleText={'نوبت دهی'}
+            loadingText={'Loading'}
+            successText={'Success'}
+            type={'button'}
+            className={'class1 class2'}
+            style={{ borderRadius: '5px'}}
+            shadow={true}
+            size={'normal'}
+            messageDuration={2000}
+            buttonRef={null}
+            animation={true}
+        />
                       </Card.Body>
                     </Card>
                   </div>
                 )
               })}
-              
+
           </div>
         </div>
       </div>
