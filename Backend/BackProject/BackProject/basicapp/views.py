3b77f60d144a864e73f3333e4d8f35153a004b71
CE9752 <fateme.ahmadi1522@gmail.com>
2021-05-13 01:40:51 +0430

    Add icons to specialties

--- Backend/BackProject/BackProject/basicapp/views.py
+++ Backend/BackProject/BackProject/basicapp/views.py
@@ -56,42 +56,42 @@ states["29"]="همدان"
 states["30"]="یزد"
 
 specialties = {}
-specialties["0"] = {"specialty":"پزشک عمومی","icon":""}
-specialties["1"] = {"specialty":"چشم پزشکی","icon":""}
-specialties["2"] = {"specialty":"دهان و دندان","icon":""}
-specialties["3"] = {"specialty":"زنان و زایمان","icon":""}
-specialties["4"] = {"specialty":"روانشناسی","icon":""}
-specialties["5"] = {"specialty":"معده و گوارش","icon":""}
-specialties["6"] = {"specialty":"پوست و مو و زیبایی","icon":""}
-specialties["7"] = {"specialty":"کلیه و مجاری ادراری","icon":""}
-specialties["8"] = {"specialty":"خون","icon":""}
-specialties["9"] = {"specialty":"ارتوپدی","icon":""}
-specialties["10"] = {"specialty":"قلب و عروق","icon":""}
-specialties["11"] = {"specialty":"اطفال","icon":""}
-specialties["12"] = {"specialty":"غدد و دیابت","icon":""}
-specialties["13"] = {"specialty":"عفونی","icon":""}
-specialties["14"] = {"specialty":"طب تسکینی و درد","icon":""}
-specialties["15"] = {"specialty":"گوش حلق و بینی","icon":""}
-specialties["16"] = {"specialty":"مغز و اعصاب","icon":""}
-specialties["17"] = {"specialty":"داخلی","icon":""}
-specialties["18"] = {"specialty":"روان پزشکی","icon":""}
-specialties["19"] = {"specialty":"داروسازی","icon":""}
-specialties["20"] = {"specialty":"جراحی","icon":""}
-specialties["21"] = {"specialty":"تغذیه","icon":""}
-specialties["22"] = {"specialty":"فیزیوتراپی","icon":""}
-specialties["23"] = {"specialty":"رادیولوژی","icon":""}
-specialties["24"] = {"specialty":"سونوگرافی","icon":""}
-specialties["25"] = {"specialty":"آزمایشگاه","icon":""}
-specialties["26"] = {"specialty":"ریه","icon":""}
-specialties["27"] = {"specialty":"ژنتیک","icon":""}
-specialties["28"] = {"specialty":"توانبخشی","icon":""}
-specialties["29"] = {"specialty":"طب سنتی","icon":""}
-specialties["30"] = {"specialty":"استخوان و مفاصل","icon":""}
-specialties["31"] = {"specialty":"بیهوشی","icon":""}
-specialties["32"] = {"specialty":"بینایی سنجی","icon":""}
-specialties["33"] = {"specialty":"شنوایی سنجی","icon":""}
-specialties["34"] = {"specialty":"آسیب شناسی","icon":""}
-specialties["35"] = {"specialty":"سایر","icon":""}
+specialties["0"] = {"specialty":"پزشک عمومی","icon":"<LocalHospitalIcon></LocalHospitalIcon>"}
+specialties["1"] = {"specialty":"چشم پزشکی","icon":"<VisibilityIcon></VisibilityIcon>"}
+specialties["2"] = {"specialty":"دهان و دندان","icon":"<FaTeeth size="25"></FaTeeth>"}
+specialties["3"] = {"specialty":"زنان و زایمان","icon":"<MdPregnantWoman size="35"></MdPregnantWoman>"}
+specialties["4"] = {"specialty":"روانشناسی","icon":"<RiPsychotherapyLine size="35"></RiPsychotherapyLine>"}
+specialties["5"] = {"specialty":"معده و گوارش","icon":"<GiStomach size="35"></GiStomach>"}
+specialties["6"] = {"specialty":"پوست و مو و زیبایی","icon":"<MdFace size="35"></MdFace>"}
+specialties["7"] = {"specialty":"کلیه و مجاری ادراری","icon":"<GiKidneys size="35"></GiKidneys>"}
+specialties["8"] = {"specialty":"خون","icon":"<IoWater size="35"></IoWater>"}
+specialties["9"] = {"specialty":"ارتوپدی","icon":"<LocalHospitalIcon></LocalHospitalIcon>"}
+specialties["10"] = {"specialty":"قلب و عروق","icon":"<GiHeartOrgan size="40"></GiHeartOrgan>"}
+specialties["11"] = {"specialty":"اطفال","icon":"<ChildCareIcon></ChildCareIcon>"}
+specialties["12"] = {"specialty":"غدد و دیابت","icon":"<LocalHospitalIcon></LocalHospitalIcon>"}
+specialties["13"] = {"specialty":"عفونی","icon":"<AiFillMedicineBox size="35"></AiFillMedicineBox>"}
+specialties["14"] = {"specialty":"طب تسکینی و درد","icon":"<IoBandageOutline size="35"></IoBandageOutline>"}
+specialties["15"] = {"specialty":"گوش حلق و بینی","icon":"<MdHearing size="35"></MdHearing>"}
+specialties["16"] = {"specialty":"مغز و اعصاب","icon":"<GiBrain size="35"></GiBrain>"}
+specialties["17"] = {"specialty":"داخلی","icon":"<IoBodyOutline size="35"></IoBodyOutline>"}
+specialties["18"] = {"specialty":"روان پزشکی","icon":"<RiPsychotherapyLine size="35"></RiPsychotherapyLine>"}
+specialties["19"] = {"specialty":"داروسازی","icon":"<GiMedicines size="35"></GiMedicines>"}
+specialties["20"] = {"specialty":"جراحی","icon":"<RiSurgicalMaskFill size="35"></RiSurgicalMaskFill>"}
+specialties["21"] = {"specialty":"تغذیه","icon":"<GiFruitBowl size="35"></GiFruitBowl>"}
+specialties["22"] = {"specialty":"فیزیوتراپی","icon":"<GiArmSling size="35"></GiArmSling>"}
+specialties["23"] = {"specialty":"رادیولوژی","icon":"<GiRadioactive size="30"></GiRadioactive>"}
+specialties["24"] = {"specialty":"سونوگرافی","icon":"<GiRadioactive size="30"></GiRadioactive>"}
+specialties["25"] = {"specialty":"آزمایشگاه","icon":"<ImLab size="25"></ImLab>"}
+specialties["26"] = {"specialty":"ریه","icon":"<RiLungsFill size="35"></RiLungsFill>"}
+specialties["27"] = {"specialty":"ژنتیک","icon":"<SiMicrogenetics size="25"></SiMicrogenetics>"}
+specialties["28"] = {"specialty":"توانبخشی","icon":"<MdAccessible size="35"></MdAccessible>"}
+specialties["29"] = {"specialty":"طب سنتی","icon":"<GiTeapotLeaves size="35"></GiTeapotLeaves>"}
+specialties["30"] = {"specialty":"استخوان و مفاصل","icon":"<GiJoint size="35"></GiJoint>"}
+specialties["31"] = {"specialty":"بیهوشی","icon":"<RiSurgicalMaskFill size="35"></RiSurgicalMaskFill>"}
+specialties["32"] = {"specialty":"بینایی سنجی","icon":"<VisibilityIcon></VisibilityIcon>"}
+specialties["33"] = {"specialty":"شنوایی سنجی","icon":"<MdHearing size="35"></MdHearing>"}
+specialties["34"] = {"specialty":"آسیب شناسی","icon":"<FaUserInjured size="35"></FaUserInjured>"}
+specialties["35"] = {"specialty":"سایر","icon":"<AiFillMedicineBox size="35"></AiFillMedicineBox>"}
 
 class RegisterView(generics.GenericAPIView):
 
