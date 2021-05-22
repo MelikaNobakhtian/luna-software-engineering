2c3ff57467e6b350a3f06b372d850dd7afd13282
CE9752 <fateme.ahmadi1522@gmail.com>
2021-05-22 15:47:01 +0430

    Create MessageModel

--- Backend/BackProject/BackProject/basicapp/models.py
+++ Backend/BackProject/BackProject/basicapp/models.py
@@ -165,3 +165,36 @@ class DialogsModel(TimeStampedModel):
     def get_dialogs_for_user(user: User):
         return DialogsModel.objects.filter(Q(user1=user) | Q(user2=user)).values_list('user1__pk', 'user2__pk')
 
+class MessageModel(TimeStampedModel, SoftDeletableModel):
+    id = models.BigAutoField(primary_key=True, verbose_name=_("Id"))
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_("Author"),
+                               related_name='from_user', db_index=True)
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_("Recipient"),
+                                  related_name='to_user', db_index=True)
+    text = models.TextField(verbose_name=_("Text"), blank=True)
+
+
+    read = models.BooleanField(verbose_name=_("Read"), default=False)
+    all_objects = models.Manager()
+
+    @staticmethod
+    def get_unread_count_for_dialog_with_user(sender, recipient):
+        return MessageModel.objects.filter(sender_id=sender, recipient_id=recipient, read=False).count()
+
+    @staticmethod
+    def get_last_message_for_dialog(sender, recipient):
+        return MessageModel.objects.filter(
+            Q(sender_id=sender, recipient_id=recipient) | Q(sender_id=recipient, recipient_id=sender)) \
+            .select_related('sender', 'recipient').first()
+
+    def __str__(self):
+        return str(self.pk)
+
+    def save(self, *args, **kwargs):
+        super(MessageModel, self).save(*args, **kwargs)
+        DialogsModel.create_if_not_exists(self.sender, self.recipient)
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = _("Message")
+        verbose_name_plural = _("Messages")
